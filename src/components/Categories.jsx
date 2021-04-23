759045be9a3068bd040e41095d093d125073668d
n.pankov <pankoff2000@gmail.com>
2021-04-23 20:58:50 +0200

    3 step. categories functional, hook

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -1,51 +1,65 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-class Categories extends React.Component {
+function Categories({ items, onClickItem }) {
+  const [activeItem, setActiveItem] = useState(null);
 
-  state = {
-    activeItem: null
+  const onSelectItem = (index) => {
+    setActiveItem(index);
   }
 
-  onSelectItem = (index) => {
-    this.setState({
-      activeItem: index
-    })
-  }
-
-  render() {
-    console.log(this.state)
-    const { items, onClickItem } = this.props;
-    return (
-      <div className="categories">
-        <ul>
-          <li>Все</li>
-          {
-            items.map((name, index) =>
-            <li
-              className={this.state.activeItem === index ? 'active' : ''}
-              onClick={() => this.onSelectItem(index)}
-              key={`${name}_${index}`}>{name}</li>
-          )}
-        </ul>
-      </div>
-    )
-  }
-}
+  return (
+    <div className="categories">
+      <ul>
+        <li
+          className={activeItem === null ? 'active' : ''}
+          onClick={() => onSelectItem(null)}>
+          Все
+        </li>
+        {items &&
+          items.map((name, index) =>
+          <li
+            className={activeItem === index ? 'active' : ''}
+            onClick={() => onSelectItem(index)}
+            key={`${name}_${index}`}>{name}
+          </li>
+        )}
+      </ul>
+    </div>
+  );
+};
 
 export default Categories;
 
-// function Categories({ items, onClickItem }) {
-//   return (
-//     <div className="categories">
-//       <ul>
-//         <li className="active">Все</li>
-//         {
-//           items.map((name, index) =>
-//           <li onClick={() => onClickItem(name)} key={`${name}_${index}`}>{name}</li>
-//         )}
-//       </ul>
-//     </div>
-//   );
-// };
-
+// class Categories extends React.Component {
+//
+//   state = {
+//     activeItem: null
+//   }
+//
+//   onSelectItem = (index) => {
+//     this.setState({
+//       activeItem: index
+//     })
+//   }
+//
+//   render() {
+//     console.log(this.state)
+//     const { items, onClickItem } = this.props;
+//     return (
+//       <div className="categories">
+//         <ul>
+//           <li>Все</li>
+//           {
+//             items.map((name, index) =>
+//             <li
+//               className={this.state.activeItem === index ? 'active' : ''}
+//               onClick={() => this.onSelectItem(index)}
+//               key={`${name}_${index}`}>{name}</li>
+//           )}
+//         </ul>
+//       </div>
+//     )
+//   }
+// }
+//
 // export default Categories;
