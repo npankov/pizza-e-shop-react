725fa1dfd8a0e8a9c38c2b5b93d4d95208db1c24
n.pankov <pankoff2000@gmail.com>
2021-04-28 20:52:13 +0200

    6.1 step. add axios

--- src/App.js
+++ src/App.js
@@ -2,14 +2,19 @@ import React, { useEffect, useState } from 'react';
 import { Route } from 'react-router-dom';
 import { Header } from './components';
 import { Home, Cart } from './pages';
+import axios from 'axios';
 
 function App() {
   const [pizzas, setPizzas] = useState([]);
 
   useEffect(() => {
-    fetch('http://localhost:3000/db.json')
-      .then((response) => response.json())
-      .then((data) => setPizzas(data.pizzas))
+    axios.get('http://localhost:3000/db.json')
+      .then(({ data }) => {
+        setPizzas(data.pizzas);
+      })
+    // fetch('http://localhost:3000/db.json')
+    //   .then((response) => response.json())
+    //   .then((data) => setPizzas(data.pizzas))
   }, [])
 
   return (
