9ae6729e65b32af83689b512e50096982d16be35
npankov <pankoff2000@gmail.com>
2021-05-02 20:14:39 +0300

    8 step. redux thunk for asynchronous get pizzas

--- src/App.js
+++ src/App.js
@@ -3,18 +3,14 @@ import { Route } from 'react-router-dom';
 import { useDispatch } from 'react-redux';
 import { Header } from './components';
 import { Home, Cart } from './pages';
-import { setPizzas } from './redux/actions/pizzas'
-import axios from 'axios';
+import { fetchPizzas } from './redux/actions/pizzas'
 
 function App() {
   const dispatch = useDispatch();
 
   useEffect(() => {
-    axios.get('http://localhost:3001/pizzas')
-      .then(({ data }) => {
-        dispatch(setPizzas(data));
-      })
-  })
+    dispatch(fetchPizzas());
+  }, [dispatch])
 
   return (
     <div className="wrapper">
