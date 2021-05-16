797b4643542eae20f305395714e219ef5577a98d
npankov <pankoff2000@gmail.com>
2021-05-16 18:22:05 +0200

    9.4 step. clear cart

--- src/redux/reducers/cart.js
+++ src/redux/reducers/cart.js
@@ -40,6 +40,23 @@ const cart = (state = initialState, action) => {
         totalCount: 0
       }
 
+    case 'REMOVE_CART_ITEM': {
+      const newItems = {
+        ...state.items
+      }
+      const currentTotalPrice = newItems[action.payload].totalPrice;
+      const currentTotalCount = newItems[action.payload].items.length;
+
+      delete newItems[action.payload];
+
+      return {
+        ...state,
+        items: newItems,
+        totalPrice: state.totalPrice - currentTotalPrice,
+        totalCount: state.totalCount - currentTotalCount
+      }
+    }
+
     default:
       return state;
   }
